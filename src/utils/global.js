a713a60d17f1230d58df910f0d672f3e9efcb852
smartbow <yangzb@smartbow.net>
2020-03-06 21:49:40 +0800

    findMenuTree感觉没用先注释掉

--- src/utils/global.js
+++ src/utils/global.js
@@ -5,8 +5,8 @@
  */
 
 // 后台管理系统服务器地址
-// export const baseUrl = 'http://localhost:8001'
-export const baseUrl = 'http://139.196.87.48:8001'
+export const baseUrl = 'http://localhost:8001'
+// export const baseUrl = 'http://139.196.87.48:8001'
 // 系统数据备份还原服务器地址
 export const backupBaseUrl = 'http://localhost:8002'
 
